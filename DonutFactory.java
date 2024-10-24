9d8de3c0127422ff1a595ae4d036928b182cba16
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-10-24 11:14:51 -0400

    Fixed donut pricing method

---
+++ DonutFactory.java
@@ -0,0 +1,30 @@
+public class DonutFactory {
+    double pricePerDonut;
+
+    public double getPricePerDonut() {
+        return pricePerDonut;
+    }
+
+    public void setPricePerDonut(double pricePerDonut) {
+        this.pricePerDonut = pricePerDonut;
+    }
+
+    public DonutFactory(double pricePerDonut) {
+        this.pricePerDonut = pricePerDonut;
+    }
+
+    double getPriceOfDonuts(int numberOfDonuts) {
+        double totalPrice = numberOfDonuts * pricePerDonut;
+
+        if (numberOfDonuts >= 72) {
+            totalPrice = totalPrice - (totalPrice * .3);
+        } else if (numberOfDonuts % 12 == 0) {
+            totalPrice = totalPrice - totalPrice * .2;
+        } else if (numberOfDonuts >= 6) {
+            totalPrice -= totalPrice * .1;
+
+
+        }
+        return totalPrice;
+    }
+}
